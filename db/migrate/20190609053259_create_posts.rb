9b6bed23976d67fcb172b25f5bc16c06bd8835c3
Rafel Villarreal <rvillarreal416@gmail.com>
2019-06-09 00:33:10 -0500

    post model updated

--- db/migrate/20190609053259_create_posts.rb
+++ db/migrate/20190609053259_create_posts.rb
@@ -4,7 +4,7 @@ class CreatePosts < ActiveRecord::Migration[5.2]
       t.string :title
       t.string :content
       t.boolean :published
-      t.reference :user
+      t.references :user, foreign_key: true
 
       t.timestamps
     end
