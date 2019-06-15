67f87f2ee01c90cb3ad7cca777565b73a8d99560
Rafael Villarreal <rvillarreal416@gmail.com>
2019-06-14 22:24:13 -0500

    auth validation for create and show controller

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,4 +1,7 @@
+require 'byebug'
 class PostsController < ApplicationController 
+
+  before_action :authenticate_user!, only: [:create, :update]
 
   rescue_from Exception do |e|
     render json: { error: e.message }, status: :internal_error 
@@ -21,18 +24,22 @@ class PostsController < ApplicationController
   #GET /posts/{id}
   def show
     @post = Post.find(params[:id])
-    render json: @post, status: :ok
+    if ( @post.published? || ( Current.user && @post.user_id == Current.user.id ))
+      render json: @post, status: :ok
+    else 
+      render json: { error: 'Not found' }, status: :not_found
+    end
   end
 
   #POST /posts
   def create
-    @post = Post.create!(create_params)
+    @post = Current.user.posts.create!(create_params)
     render json: @post, status: :created
   end
 
   #PUT /posts/{id}
   def update
-    @post = Post.find(params[:id])
+    @post = Current.user.posts.find(params[:id])
     @post.update!(update_params)
     render json: @post, status: :ok
   end
@@ -40,10 +47,27 @@ class PostsController < ApplicationController
   private
 
   def create_params
-    params.require(:post).permit(:title, :content, :published, :user_id)
+    params.require(:post).permit(:title, :content, :published)
   end
 
   def update_params
     params.require(:post).permit(:title, :content, :published)
+  end
+
+  def authenticate_user!
+    # read auth header
+    token_regex = /Bearer (\w+)/
+    headers = request.headers
+    # check if  auth header is valid
+    if headers['Authorization'].present? && headers['Authorization'].match(token_regex)
+      token = headers['Authorization'].match(token_regex)[1]
+      # check if token is realted to a user
+      if ( Current.user = User.find_by_auth_token(token) ) 
+        return 
+      end
+
+    end
+
+    render json: { error: 'Unauthorized', status: :unauthorized }
   end
 end
